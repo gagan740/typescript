7a8577b39f41010e37f2184b1fbb2e98fd8a4ff8
gagan740 <dgagan989@gmail.com>
2017-07-17 19:04:31 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,23 @@
+// var myName: string      =   "Gagan";
+// var myAge: number       =   23;
+// var canVote: boolean    =   true;
+// var anything: any       =   "dog";
+// anything                =   2;
+// document.getElementById("tsStuff").innerHTML    =   "My name is " + myName;
+// document.write("myname is a " + typeof(myName) + "<br/>");
+// document.write("anything is a " + typeof(anything) + "<br/>");
+// var strToNum: number    =   parseInt("5");
+// var numToStr: number    =   5;
+// document.write("numToStr is a " + typeof(numToStr.toString())+"<br/>");
+// const PI    =   3.14159;
+// interface SuperHero{
+//     realName: string;
+//     superName: string;
+// }
+// var superman: SuperHero =   {
+//     realName: "Tony Stark",
+//     superName: "Iron Man"
+// }
+// document.write(superman.realName + " is " + superman.superName + "<br/>");
+var employees = ["Gourav", "Manak", "Yashpal"];
+document.write(employees.toString() + "<br>");
