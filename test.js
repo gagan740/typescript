f9a73018fe8375aa3650a7437eef23f99a5a8757
gagan740 <dgagan989@gmail.com>
2017-07-19 11:30:33 +0530

    updates

--- test.js
+++ test.js
@@ -19,5 +19,61 @@
 //     superName: "Iron Man"
 // }
 // document.write(superman.realName + " is " + superman.superName + "<br/>");
-var employees = ["Gourav", "Manak", "Yashpal"];
-document.write(employees.toString() + "<br>");
+// var employees: string[]   = ["Gourav","Manak","Yashpal"];
+// document.write(employees.toString() + "<br>");
+// interface SuperHero{
+//     realName: string;
+//     superName: string;
+// }
+// var superheros: SuperHero[] = [];
+// superheros.push({
+//     realName: 'Tony Stark',
+//     superName: 'Iron Man'
+// });
+// document.write(superheros[0].realName + " is " + superheros[0].superName + "<br/>");
+// document.write("5+4 = " + (5+4) + "<br/>");
+// var randArray   =   [1,2,3,4,];
+// for(var val in randArray){
+//     document.write(val + "<br/>");
+// }
+// var strArray    =    randArray.map(String);
+// for(var val of strArray){
+//      document.write(val + "<br/>");
+// }
+// var getSum      =   function (num1:number, num2:number):number{
+//     return num1+num2;
+// }
+// var theSum1: number     =   getSum(9,5);
+// document.write(theSum1);
+// var addOne  =   (x)=>x+1;
+// document.write("1 + 1 = " + addOne(1) + "<br/>");
+var Animal = (function () {
+    function Animal(name, owner) {
+        this.name = name;
+        this.owner = owner;
+        Animal.numOfAnimals++;
+    }
+    Animal.prototype.ownerInfo = function () {
+        document.write(this.name + " is owned by " + this.owner + "<br/>");
+    };
+    Animal.howManyAnimals = function () {
+        return Animal.numOfAnimals;
+    };
+    Object.defineProperty(Animal.prototype, "weight", {
+        get: function () {
+            return this._weight;
+        },
+        set: function (weight) {
+            this._weight = weight;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Animal.numOfAnimals = 0;
+    return Animal;
+}());
+var spot = new Animal("Spot", "Doug");
+spot.ownerInfo();
+spot.weight = 100;
+document.write("Spot's weight is " + spot.weight + "<br/>");
+document.write("# of Animals : " + Animal.howManyAnimals() + "<br/>");
--- test.ts
+++ test.ts
@@ -28,6 +28,82 @@
 
 // document.write(superman.realName + " is " + superman.superName + "<br/>");
 
-var employees: string[]   = ["Gourav","Manak","Yashpal"];
+// var employees: string[]   = ["Gourav","Manak","Yashpal"];
 
-document.write(employees.toString() + "<br>");
+// document.write(employees.toString() + "<br>");
+
+// interface SuperHero{
+//     realName: string;
+//     superName: string;
+// }
+
+// var superheros: SuperHero[] = [];
+
+// superheros.push({
+//     realName: 'Tony Stark',
+//     superName: 'Iron Man'
+// });
+
+// document.write(superheros[0].realName + " is " + superheros[0].superName + "<br/>");
+
+// document.write("5+4 = " + (5+4) + "<br/>");
+
+// var randArray   =   [1,2,3,4,];
+
+// for(var val in randArray){
+//     document.write(val + "<br/>");
+// }
+
+// var strArray    =    randArray.map(String);
+// for(var val of strArray){
+//      document.write(val + "<br/>");
+// }
+
+// var getSum      =   function (num1:number, num2:number):number{
+//     return num1+num2;
+// }
+
+// var theSum1: number     =   getSum(9,5);
+
+// document.write(theSum1);
+
+// var addOne  =   (x)=>x+1;
+// document.write("1 + 1 = " + addOne(1) + "<br/>");
+
+class Animal{
+    public favFood: string;
+
+    static numOfAnimals: number =   0;
+
+    constructor(private name: string, private owner: string){
+        Animal.numOfAnimals++;
+    }
+
+    ownerInfo(){
+        document.write(this.name + " is owned by " + this.owner + "<br/>");
+    }
+
+    static howManyAnimals(): number{
+         return Animal.numOfAnimals;
+    }
+
+    private _weight: number;
+
+    get weight():number{
+        return this._weight;
+    }
+
+    set weight(weight: number){
+        this._weight    =   weight;
+    }
+}
+
+var spot    =   new Animal("Spot","Doug");
+
+spot.ownerInfo();
+
+spot.weight     =   100;
+
+document.write("Spot's weight is " + spot.weight + "<br/>" );
+
+document.write("# of Animals : " + Animal.howManyAnimals() + "<br/>" );
